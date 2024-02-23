6d18a519cab509ead4d76d7ae5fc6ed9405505d5
mahmoudhafez103 <hafz7062@gmail.com>
2024-02-23 23:12:49 +0200

    my first upload

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<html>
+<title>hello world</title>
+<link rel="stylesheet" href="css/app.css">
+<body>
+<h1>hi mahmoud</h1>
+<article>
+<?= $post;
+    ?></article>
+<a href="/">go home</a>
+</body>
+
+
+</html>
