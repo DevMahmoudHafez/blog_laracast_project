c41345d168072dbad1e8ad4665cdb6d3b7f5089c
mahmoudhafez103 <hafz7062@gmail.com>
2024-06-27 12:57:40 +0300

    edits for production

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,13 +1,12 @@
 @props(['post'])
 <article
-    {{$attributes->merge(['class'=>'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl'])}}>
+    {{ $attributes->merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl']) }}>
     <div class="py-6 px-5">
         <div>
-            <img
-                @unless(isset($post->thumbnail))
-                    src="/images/illustration-3.png"
+            <img @unless (isset($post->thumbnail))
+                    src="{{ asset('/images/illustration-3.png') }}"
                 @else
-                    src="{{asset('storage/'.$post->thumbnail)}}"
+                    src="{{ asset('storage/' . $post->thumbnail) }}"
                 @endunless
                 alt="Blog Post illustration" class="rounded-xl">
         </div>
@@ -18,14 +17,14 @@
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        <a href="/posts/{{$post->slug}}">
-                            {{$post->title}}
+                        <a href="/posts/{{ $post->slug }}">
+                            {{ $post->title }}
 
                         </a>
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
-                        Published <time>{{$post->created_at->diffForHumans()}}</time>
+                        Published <time>{{ $post->created_at->diffForHumans() }}</time>
                     </span>
                 </div>
             </header>
@@ -36,16 +35,15 @@
 
                 </p>
 
-
             </div>
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="{{ asset('/images/lary-avatar.svg') }}" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">
-                            <a href="/?author={{$post->author->username}}">
-                                {{$post->author->name}}
+                            <a href="/?author={{ $post->author->username }}">
+                                {{ $post->author->name }}
                             </a>
 
                         </h5>
@@ -54,9 +52,9 @@
                 </div>
 
                 <div>
-                    <a href="/posts/{{$post->slug}}"
-                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
-                    >Read More</a>
+                    <a href="/posts/{{ $post->slug }}"
+                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
+                        More</a>
                 </div>
             </footer>
         </div>
